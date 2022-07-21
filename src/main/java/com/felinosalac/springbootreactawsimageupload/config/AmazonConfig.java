0426e0870936ec2ba128c705666a091b3bbb304a
Felino Salac II <felinosalac@gmail.com>
2022-07-21 19:49:21 +0800

    fixed security issues, by applying externalization of aws access key id and security key.

--- src/main/java/com/felinosalac/springbootreactawsimageupload/config/AmazonConfig.java
+++ src/main/java/com/felinosalac/springbootreactawsimageupload/config/AmazonConfig.java
@@ -5,16 +5,24 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AmazonConfig {
 	
+	@Value("${amazon.botsalac.access_key_id}")
+    private String awsAccessKeyId;
+	
+	@Value("${amazon.botsalac.secret_access_key}")
+    private String awsSecretKey;
+	
 	
 	@Bean
 	public AmazonS3 s3() {
-		AWSCredentials awsCredentials = new BasicAWSCredentials("", "");
+		AWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessKeyId, awsSecretKey);
 
 		return AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
 				.build();
