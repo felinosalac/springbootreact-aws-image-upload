3294a06073601b7e6f03fde45bb41ae05c83ca26
Felino Salac II <felinosalac@gmail.com>
2022-07-21 12:34:53 +0800

    initial commit

---
+++ src/main/java/com/felinosalac/springbootreactawsimageupload/SpringbootreactAwsImageUploadApplication.java
@@ -0,0 +1,13 @@
+package com.felinosalac.springbootreactawsimageupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootreactAwsImageUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootreactAwsImageUploadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felinosalac/springbootreactawsimageupload/SpringbootreactAwsImageUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felinosalac.springbootreactawsimageupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootreactAwsImageUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
